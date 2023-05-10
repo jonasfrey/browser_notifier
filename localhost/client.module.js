e72282e17c09d3b917d0ce8aad1a01b2f236de93
jf18j492 <jonas.immanuel.frey@golog.ch>
2023-05-10 16:55:04 +0200

    new

--- localhost/client.module.js
+++ localhost/client.module.js
@@ -1,51 +1,14 @@
 // import {f_o_html_from_o_js} from "https://deno.land/x/f_o_html_from_o_js@0.7/mod.js";
 import {f_o_html_from_o_js} from "./f_o_html_from_o_js.module.js";
 
-import {f_add_css} from "https://deno.land/x/f_add_css@0.3"
+import {f_add_css} from "https://deno.land/x/f_add_css@0.3/mod.js"
 
 let o_state = {
     a_o_notification: [],
     o_js__a_o_notification: [],
     a_o_js_o_notification: []
 };
-let s_css = `
-.a_o_notification .info,
-.a_o_notification .success,
-.a_o_notification .warning,
-.a_o_notification .error,
-.a_o_notification .validation {
-    border: 1px solid;
-    margin: 10px 0px;
-    padding: 15px 10px 15px 50px;
-    background-repeat: no-repeat;
-    background-position: 10px center;
-}
-.a_o_notification .info {
-    color: #00529B !important;
-    background-color: #BDE5F8 !important;
-    background-image: url('https://i.imgur.com/ilgqWuX.png');
-}
-.a_o_notification .success {
-    color: #4F8A10 !important;
-    background-color: #DFF2BF !important;
-    background-image: url('https://i.imgur.com/Q9BGTuy.png');
-}
-.a_o_notification .warning {
-    color: #9F6000 !important;
-    background-color: #FEEFB3 !important;
-    background-image: url('https://i.imgur.com/Z8q7ww7.png');
-}
-.a_o_notification .error{
-    color: #D8000C !important;
-    background-color: #FFBABA !important;
-    background-image: url('https://i.imgur.com/GnyDvKN.png');
-}
-.a_o_notification .validation{
-    color: #D63301 !important;
-    background-color: #FFCCBA !important;
-    background-image: url('https://i.imgur.com/GnyDvKN.png');
-}
-`;
+
 f_add_css('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css')
 
 class O_notification{
@@ -97,7 +60,7 @@ let f_o_notification = function(
         }).innerText.length / n_chars_per_word;
         var n_seconds_for_msg = n_words * n_seconds_per_word;
         // console.log(n_seconds_for_msg)
-        n_seconds_for_msg = Math.max(2, n_seconds_for_msg)
+        n_seconds_for_msg = Math.max(3, n_seconds_for_msg)
         n_milliseconds_to_live = n_seconds_for_msg*1000
     }
 
@@ -162,6 +125,144 @@ let f_o_notification = function(
 
     
 }
+let f_f_o_js__font_awesome_icon = function(
+    s_msg, 
+    s_class__icon = "fa-solid fa-circle-exclamation",
+    s_class = "info"
+){
+
+    let f = function(
+        o_notification
+    ){
+        if(s_class__icon.includes("spin")){
+            //infinity timeout
+            o_notification.n_milliseconds_to_live = o_notification.n_milliseconds_to_live__constructor;
+        }
+        return {
+            ...(function(){
+                if(s_class__icon.includes("spin")== false){
+                    return {
+                        onmouseenter: function(){
+                            window.cancelAnimationFrame(o_notification.n_animation_id)
+                        }, 
+                        onmouseleave: function(){
+                            o_notification.n_animation_id = window.requestAnimationFrame(o_notification.f_render)
+                            // o_notification.n_ms_wind_perf_now = window.performance.now();
+                            o_notification.n_ms_wind_perf_now__last = window.performance.now();
+                        },
+                    }
+                }
+            })(),
+            class: [
+                'o_notification', 
+                s_class
+            ].join(" "), 
+            style: [
+                'display: flex',
+                'flex-wrap:wrap',
+                'flex-direction:row',
+                'position:relative',
+            ].join(";"),
+            a_o:[
+                {
+                    style: 'padding-right: 0.5rem',
+                    a_o: [
+                        {
+                            style: [
+                                (function(){
+                                    if(s_class__icon.includes("spin")){
+                                        return `transform:rotate(${window.performance.now()*0.2}deg);`  
+                                    }
+                                })()
+                            ].join(';'),
+                            class: s_class__icon, 
+                        }
+                    ]
+    
+                },
+                {
+                    innerText: s_msg
+                },
+                {
+                    b_render: s_class__icon.includes("spin") == false,
+                    class: "fa-solid fa-xmark", 
+                    // s_tag: "button", 
+                    // innerHTML: "X", 
+                    style: [
+                        'position:absolute',
+                        'top:5px', 
+                        'right: 5px',
+                        'font-size:0.9rem'
+                    ].join(";"),
+                    onmousedown: function(){
+                        o_notification.n_milliseconds_to_live = -1;
+                        o_notification.b_render = false;
+                        o_notification.o_state.o_js__a_o_notification._f_render();
+                    }
+                }, 
+                {
+                    b_render: s_class__icon.includes("spin") == false,
+
+                    style: [
+                        'position:absolute',
+                        'bottom:0', 
+                        'left: 0',
+                        (function(){
+                            let n_nor = o_notification.n_milliseconds_to_live / o_notification.n_milliseconds_to_live__constructor
+                        
+                            return `width:${n_nor*100}%`
+                        })(),
+                        'height:5px',
+                        `background:var(--color-foreground-${s_class})`
+                    ].join(";"),
+                }
+            ]
+        }
+    }
+    return f
+}
+let f_f_o_js__loading = function(s_text){
+    return f_f_o_js__font_awesome_icon(
+        s_text,
+        "fa-solid fa-spinner spin", 
+        "loading"
+    )
+}
+let f_f_o_js__info = function(s_text){
+    return f_f_o_js__font_awesome_icon(
+        s_text,
+        "fa-solid fa-circle-exclamation", 
+        "info"
+    )
+}
+let f_f_o_js__success = function(s_text){
+    return f_f_o_js__font_awesome_icon(
+        s_text,
+        "fa-solid fa-circle-check", 
+        "success"
+    )
+}
+let f_f_o_js__warning = function(s_text){
+    return f_f_o_js__font_awesome_icon(
+        s_text,
+        "fa-solid fa-triangle-exclamation", 
+        "warning"
+    )
+}
+let f_f_o_js__error = function(s_text){
+    return f_f_o_js__font_awesome_icon(
+        s_text,
+        "fa-sharp fa-solid fa-circle-exclamation", 
+        "error"
+    )
+}
+let f_f_o_js__validation = function(s_text){
+    return f_f_o_js__font_awesome_icon(
+        s_text,
+        "asdf", 
+        "validation"
+    )
+}
 let f_o_js__timeout_bar = function(o_notification){
     // console.log("asdf")
     return {
@@ -220,7 +321,11 @@ let f_f_o_js__timeoutbar_and_close_button = function(s_msg){
     return f_o_js
 }
 
-
+let f_clear_all_notifications = function(){
+    o_state.a_o_notification = [];
+    o_state.a_o_js_o_notification = [];
+    o_state.o_js__a_o_notification._f_render()
+}
 
 
 
@@ -255,11 +360,41 @@ let f_o_js__notifier = function(){
         ],
     };
 
-    f_add_css(
-        `${import.meta.url.split('/').slice(0, -1).join("/")}/./pico_browser_notifier.scss.css`
-    )
-    f_add_css(
+    // f_add_css(
+    //     `${import.meta.url.split('/').slice(0, -1).join("/")}/./pico_browser_notifier.scss.css`
+    // )
+    let o_s_state_o_color =  {
+        loading: {
+            s_color_foreground:'#00529B', 
+            s_color_background:'#BDE5F8',
+        },
+        info: {
+            s_color_foreground:'#00529B', 
+            s_color_background:'#BDE5F8',
+        },
+        success: {
+            s_color_foreground:'#4F8A10', 
+            s_color_background:'#DFF2BF',
+        },
+        warning: {
+            s_color_foreground:'#9F6000', 
+            s_color_background:'#FEEFB3',
+        },
+        error: {
+            s_color_foreground:'#D8000C', 
+            s_color_background:'#FFBABA',
+        },
+        validation: {
+            s_color_foreground:'#D63301', 
+            s_color_background:'#FFCCBA',
+        },
+    }
+    let s_css = (
         `
+        .a_o_notification{
+            font-family: sans;
+            font-size: 1.2rem;
+        }
         .a_o_notification{
             padding:1rem;
             position: fixed;
@@ -277,9 +412,32 @@ let f_o_js__notifier = function(){
             background: var(--card-background-color);
             box-shadow: var(--card-box-shadow)
         }
+        :root{
+            ${Object.keys(o_s_state_o_color).map(function(s){
+                return `
+                --color-foreground-${s}: ${o_s_state_o_color[s].s_color_foreground};
+                --color-background-${s}: ${o_s_state_o_color[s].s_color_background};
+                `
+            }).join(';')}
+        }
+        ${Object.keys(o_s_state_o_color).map(function(s){
+            return `
+            .${s}{
+                color: var(--color-foreground-${s}); 
+                background: var(--color-background-${s}); 
+            }
+            `
+        }).join("\n")}
+        .o_notification{
+            max-width:500px;
+            border: 1px solid;
+            border-radius: 2px;
+            margin: 10px 0px;
+            padding: 1rem;
+        }
         `
     )
-    
+    f_add_css(s_css)
 
     return o;
 
@@ -306,5 +464,13 @@ export {
     f_o_js__timeout_bar, 
     f_f_o_js__timeoutbar_and_close_button, 
     f_o_notification, 
-    f_o_notification__and_push_and_render
+    f_o_notification__and_push_and_render, 
+    f_f_o_js__font_awesome_icon, 
+    f_clear_all_notifications,
+    f_f_o_js__loading,
+    f_f_o_js__info,
+    f_f_o_js__success,
+    f_f_o_js__warning,
+    f_f_o_js__error,
+    f_f_o_js__validation
 }
